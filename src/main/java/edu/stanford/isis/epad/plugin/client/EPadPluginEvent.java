15d80e51e4db43461f545f503d9df7281da55b8d
martin <martin@zippyrate.com>
2013-07-16 15:50:37 -0700

    Initial version of common code; contains only plugin code for the moment; pre Restlet

---
+++ src/main/java/edu/stanford/isis/epad/plugin/client/EPadPluginEvent.java
@@ -0,0 +1,12 @@
+package edu.stanford.isis.epad.plugin.client;
+
+import com.google.gwt.event.shared.EventHandler;
+
+/**
+ * EPadPluginEvent
+ *
+ * @author amsnyder
+ */
+public class EPadPluginEvent<H extends EventHandler> {
+
+}
---
+++ src/main/java/edu/stanford/isis/epad/plugin/client/PluginEventHandler.java
@@ -0,0 +1,13 @@
+package edu.stanford.isis.epad.plugin.client;
+
+/**
+ * PluginEventHandler
+ *
+ * @author amsnyder
+ */
+public interface PluginEventHandler {
+
+
+    void onPluginEvent(EPadPluginEvent event);
+
+}
