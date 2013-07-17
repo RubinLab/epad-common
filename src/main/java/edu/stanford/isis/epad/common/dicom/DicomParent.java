1ebadfd5f59ea46fd54e061ace284d51c48b511a
martin <martin@zippyrate.com>
2013-07-17 16:47:55 -0700

    Moved a lot of code to epad-common

---
+++ src/main/java/edu/stanford/isis/epad/common/dicom/DicomParent.java
@@ -0,0 +1,27 @@
+package edu.stanford.isis.epad.common.dicom;
+
+
+/**
+ * Holds a Dicom UID and the type.
+ *
+ * @author amsnyder
+ */
+public class DicomParent {
+
+    final String dicomUID;
+    final DicomParentType type;
+
+    public DicomParent(String dicomUID, DicomParentType type){
+        this.dicomUID = dicomUID;
+        this.type = type;
+    }
+
+    public String getDicomUID(){
+        return dicomUID;
+    }
+
+    public DicomParentType getType(){
+        return type;
+    }
+
+}
---
+++ src/main/java/edu/stanford/isis/epad/common/dicom/DicomParentType.java
@@ -0,0 +1,13 @@
+package edu.stanford.isis.epad.common.dicom;
+
+/**
+ * @author amsnyder
+ */
+public enum DicomParentType {
+
+    IMAGE,
+    SERIES,
+    STUDY,
+    PATIENT;
+
+}
