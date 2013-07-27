94165976541237550c7946ee0140f7fe2356f921
martin <martin@zippyrate.com>
2013-07-26 20:19:44 -0700

    More DICOM window levelling cleanup

--- src/main/java/edu/stanford/isis/epad/common/dicom/DicomFormatUtil.java
+++ src/main/java/edu/stanford/isis/epad/common/dicom/DicomFormatUtil.java
@@ -7,83 +7,92 @@
  */
 package edu.stanford.isis.epad.common.dicom;
 
-
 import java.io.File;
 
 /**
  * Utility to formatUidToDir UID in directories.
- *
+ * 
  * @author amsnyder
  */
-public class DicomFormatUtil {
+public class DicomFormatUtil
+{
+	private DicomFormatUtil()
+	{
+	}
 
-    private DicomFormatUtil(){}
+	// TODO Get this from a configuration properties.
+	public static final String DICOM_FILE_PATH = "/Users/martin/tmp/resources/dicom";
 
-    public static final String DICOM_FILE_PATH = "../resources/dicom";
+	/**
+	 * For a UID from 1.2.34.567 formatUidToDir into 1_2_34_567 formatUidToDir.
+	 * 
+	 * @param dicomUID String Expects UID in 1.2.34.567 format
+	 * @return String UID in 1_2_34_567 format
+	 */
+	public static String formatUidToDir(String dicomUID)
+	{
+		return dicomUID.replace('.', '_');
+	}
 
-    /**
-     * For a UID from 1.2.34.567 formatUidToDir into 1_2_34_567 formatUidToDir.
-     * @param dicomUID String Expects UID in 1.2.34.567 format
-     * @return String UID in 1_2_34_567 format
-     */
-    public static String formatUidToDir(String dicomUID)
-    {
-        return dicomUID.replace('.','_');
-    }
+	public static String formatDirToUid(String dicomDir)
+	{
+		return dicomDir.replace('_', '.');
+	}
 
-    public static String formatDirToUid(String dicomDir){
-        return dicomDir.replace('_','.');
-    }
+	/**
+	 * Get ../resources/dicom which is the base file path.
+	 * 
+	 * @return String
+	 */
+	public static String getDicomBaseDirPath()
+	{
+		return DICOM_FILE_PATH;
+	}
 
-    /**
-     * Get ../resources/dicom which is the base file path.
-     * @return String
-     */
-    public static String getDicomBaseDirPath(){
-        return DICOM_FILE_PATH;
-    }
+	public static String createDicomDirPath(String studyUID)
+	{
+		String studyDir = formatUidToDir(studyUID);
+		return DICOM_FILE_PATH + "/" + studyDir;
+	}
 
-    public static String createDicomDirPath(String studyUID){
-        String studyDir = formatUidToDir(studyUID);
-        return DICOM_FILE_PATH+"/"+studyDir;
-    }
+	public static String createDicomDirPath(String studyUID, String seriesUID)
+	{
+		return createDicomDirPath(studyUID) + "/" + formatUidToDir(seriesUID);
+	}
 
-    public static String createDicomDirPath(String studyUID, String seriesUID){
-        return createDicomDirPath(studyUID)+"/"+formatUidToDir(seriesUID);
-    }
+	public static String createOrderFilePath(String studyUID, String seriesUID)
+	{
+		return createDicomDirPath(studyUID) + "/series_" + formatUidToDir(seriesUID) + ".order";
+	}
 
-    public static String createOrderFilePath(String studyUID, String seriesUID){
-        return createDicomDirPath(studyUID)+"/series_"+formatUidToDir(seriesUID)+".order";
-    }
+	public static boolean hasSeriesDir(String studyUID, String seriesUID)
+	{
+		String checkPath = DICOM_FILE_PATH + "/" + formatUidToDir(studyUID) + "/" + formatUidToDir(seriesUID);
+		File checkFile = new File(checkPath);
 
-    public static boolean hasSeriesDir(String studyUID, String seriesUID){
-        String checkPath = DICOM_FILE_PATH+"/"+formatUidToDir(studyUID)
-                +"/"+formatUidToDir(seriesUID);
-        File checkFile = new File(checkPath);
+		return checkFile.exists();
+	}
 
-        return checkFile.exists();
-    }
+	/**
+	 * Return a true if a file of this type exists.
+	 * 
+	 * @return boolean
+	 */
+	public static boolean hasFileWithExtension(String studyUID, String seriesUID, String sopInstanceUID, String extension)
+	{
+		if (!hasSeriesDir(studyUID, seriesUID)) {
+			return false;
+		}
 
-    /**
-     * Return a true if a file of this type exists.
-     * @return boolean
-     */
-    public static boolean hasFileWithExtension(String studyUID, String seriesUID,
-                                               String sopInstanceUID, String extension)
-    {
-        if( !hasSeriesDir(studyUID,seriesUID) ){
-            return false;
-        }
+		if (!extension.startsWith(".")) {
+			extension = "." + extension;
+		}
 
-        if( !extension.startsWith(".") ){
-            extension = "."+extension;
-        }
+		String checkFilePath = DICOM_FILE_PATH + "/" + formatUidToDir(studyUID) + "/" + formatUidToDir(seriesUID) + "/"
+				+ formatUidToDir(sopInstanceUID) + extension;
+		File checkFile = new File(checkFilePath);
 
-        String checkFilePath = DICOM_FILE_PATH+"/"+formatUidToDir(studyUID)
-                +"/"+formatUidToDir(seriesUID)+"/"+formatUidToDir(sopInstanceUID)+extension;
-        File checkFile = new File(checkFilePath);
-
-        return checkFile.exists();
-    }
+		return checkFile.exists();
+	}
 
 }
