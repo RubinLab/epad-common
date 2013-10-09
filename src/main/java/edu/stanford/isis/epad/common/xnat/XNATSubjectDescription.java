7350a2db864da4a1ffbaa7c5770fc00819523aa8
martin <martin@zippyrate.com>
2013-10-09 10:22:50 -0700

    Minor cleanup

--- src/main/java/edu/stanford/isis/epad/common/xnat/XNATSubjectDescription.java
+++ src/main/java/edu/stanford/isis/epad/common/xnat/XNATSubjectDescription.java
@@ -3,7 +3,7 @@ package edu.stanford.isis.epad.common.xnat;
 /**
  * A description of an XNAT subject.
  * <p>
- * <code>curl -b JSESSIONID=[session_key] -X GET "http:[host:port]/projects/[project_id]/subject/"</code>
+ * <code>curl -b JSESSIONID=[session_key] -X GET "http:[host:port]/projects/[project_id]/subjects/"</code>
  * <p>
  * Redirects to the XNAT call <code>/xnat/data/projects/..</code>, which returns a JSON-specified list of subjects for
  * the specified project, e.g.,
