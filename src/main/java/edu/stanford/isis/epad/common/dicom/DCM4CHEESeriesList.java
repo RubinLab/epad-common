80b1bb9eeacdec0b1ae471273a66dd7ad93c4c3e
martin <martin@zippyrate.com>
2014-03-04 14:49:52 -0800

    Added more querying functionality

---
+++ src/main/java/edu/stanford/isis/epad/common/dicom/DCM4CHEESeriesList.java
@@ -0,0 +1,39 @@
+package edu.stanford.isis.epad.common.dicom;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+public class DCM4CHEESeriesList
+{
+	public final List<DCM4CHEESeries> ResultSet; // TODO Move to lower case but sync with front end call first
+
+	public DCM4CHEESeriesList()
+	{
+		this.ResultSet = new ArrayList<DCM4CHEESeries>();
+	}
+
+	public DCM4CHEESeriesList(List<DCM4CHEESeries> resultSet)
+	{
+		this.ResultSet = Collections.unmodifiableList(resultSet);
+	}
+
+	public void addDCM4CHEESeriesDescription(DCM4CHEESeries dicomSeriesDescription)
+	{
+		ResultSet.add(dicomSeriesDescription);
+	}
+
+	public int getNumberOfStudies()
+	{
+		return this.ResultSet.size();
+	}
+
+	public String toJSON()
+	{
+		Gson gson = new Gson();
+
+		return gson.toJson(this);
+	}
+}
---
+++ src/main/java/edu/stanford/isis/epad/common/dicom/DCM4CHEEStudyList.java
@@ -0,0 +1,44 @@
+package edu.stanford.isis.epad.common.dicom;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+/**
+ * Description of a list of studies returned from the DCM4CHEE database.
+ * 
+ * @author martin
+ */
+public class DCM4CHEEStudyList
+{
+	public final List<DCM4CHEEStudy> ResultSet; // TODO Move to lower case but sync with front end call first
+
+	public DCM4CHEEStudyList()
+	{
+		this.ResultSet = new ArrayList<DCM4CHEEStudy>();
+	}
+
+	public DCM4CHEEStudyList(List<DCM4CHEEStudy> resultSet)
+	{
+		this.ResultSet = Collections.unmodifiableList(resultSet);
+	}
+
+	public void addDICOMStudyDescription(DCM4CHEEStudy dicomStudyDescription)
+	{
+		ResultSet.add(dicomStudyDescription);
+	}
+
+	public int getNumberOfStudies()
+	{
+		return this.ResultSet.size();
+	}
+
+	public String toJSON()
+	{
+		Gson gson = new Gson();
+
+		return gson.toJson(this);
+	}
+}
