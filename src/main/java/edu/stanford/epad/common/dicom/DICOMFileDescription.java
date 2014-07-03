5fd2670aa90d18b2bbfd7c86eace0ed7f3b7ddb3
martin <martin@zippyrate.com>
2014-07-03 14:25:29 -0700

    Minor refactoring

--- src/main/java/edu/stanford/epad/common/dicom/DICOMFileDescription.java
+++ src/main/java/edu/stanford/epad/common/dicom/DICOMFileDescription.java
@@ -17,4 +17,55 @@ public class DICOMFileDescription
 		this.filePath = filePath;
 		this.fileSize = fileSize;
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.filePath == null) ? 0 : this.filePath.hashCode());
+		result = prime * result + this.fileSize;
+		result = prime * result + ((this.imageUID == null) ? 0 : this.imageUID.hashCode());
+		result = prime * result + this.instanceNumber;
+		result = prime * result + ((this.seriesUID == null) ? 0 : this.seriesUID.hashCode());
+		result = prime * result + ((this.studyUID == null) ? 0 : this.studyUID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DICOMFileDescription other = (DICOMFileDescription)obj;
+		if (this.filePath == null) {
+			if (other.filePath != null)
+				return false;
+		} else if (!this.filePath.equals(other.filePath))
+			return false;
+		if (this.fileSize != other.fileSize)
+			return false;
+		if (this.imageUID == null) {
+			if (other.imageUID != null)
+				return false;
+		} else if (!this.imageUID.equals(other.imageUID))
+			return false;
+		if (this.instanceNumber != other.instanceNumber)
+			return false;
+		if (this.seriesUID == null) {
+			if (other.seriesUID != null)
+				return false;
+		} else if (!this.seriesUID.equals(other.seriesUID))
+			return false;
+		if (this.studyUID == null) {
+			if (other.studyUID != null)
+				return false;
+		} else if (!this.studyUID.equals(other.studyUID))
+			return false;
+		return true;
+	}
 }
