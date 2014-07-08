d656545c5c48d87b8c2443743ebdae71a837f10c
martin <martin@zippyrate.com>
2014-07-08 10:10:42 -0700

    Enhanced dcm4chee image description result

---
+++ src/main/java/edu/stanford/epad/common/dicom/DCM4CHEEImageDescription.java
@@ -0,0 +1,22 @@
+package edu.stanford.epad.common.dicom;
+
+public class DCM4CHEEImageDescription
+{
+	public final String studyUID, seriesUID, imageUID;
+	public final int instanceNumber;
+	public final String sliceLocation, contentTime, updatedTime, createdTime, classUID;
+
+	public DCM4CHEEImageDescription(String studyUID, String seriesUID, String imageUID, int instanceNumber,
+			String sliceLocation, String contentTime, String updatedTime, String createdTime, String classUID)
+	{
+		this.studyUID = studyUID;
+		this.seriesUID = seriesUID;
+		this.imageUID = imageUID;
+		this.instanceNumber = instanceNumber;
+		this.sliceLocation = sliceLocation;
+		this.contentTime = contentTime;
+		this.updatedTime = updatedTime;
+		this.createdTime = createdTime;
+		this.classUID = classUID;
+	}
+}
