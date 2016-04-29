516293e4d81776cacbaa436e7ec35075be474b82
Emel Alkim <emelalkim@msn.com>
2016-04-29 12:33:59 -0700

    Revert "dso aim image ref entity to dso removed (tedseg)"
    This reverts commit 4a002d3432833bbd8ab63a9f805b74db5fc27c40.

--- src/main/java/edu/stanford/epad/common/plugins/PluginAIMUtil.java
+++ src/main/java/edu/stanford/epad/common/plugins/PluginAIMUtil.java
@@ -214,10 +214,9 @@ public class PluginAIMUtil
 		sc.AddSegmentation(new edu.stanford.hakan.aim4api.compability.aimv3.Segmentation(0, imageUID, sopClassUID, sourceImageUID, 1));
 		
 		imageAnnotation.setSegmentationCollection(sc);
-		//ml remove adding the second image ref
-//		edu.stanford.hakan.aim4api.compability.aimv3.DICOMImageReference dicomImageReference = createDICOMImageReferenceV3Compability(studyUID, seriesUID,
-//				imageUID, sopClassUID);
-//		imageAnnotation.addImageReference(dicomImageReference);
+		edu.stanford.hakan.aim4api.compability.aimv3.DICOMImageReference dicomImageReference = createDICOMImageReferenceV3Compability(studyUID, seriesUID,
+				imageUID);
+		imageAnnotation.addImageReference(dicomImageReference);
 		
 		updateDSOAIMInDatabase(imageAnnotation.getUniqueIdentifier(), seriesUID);
 		return imageAnnotation.toAimV4();
@@ -235,37 +234,7 @@ public class PluginAIMUtil
 		return edu.stanford.hakan.aim4api.usage.AnnotationGetter.getImageAnnotationCollectionFromFile(getRealPath(file), xsdFilePathV4);
 	}
         
-        //ml added sopclassuid from image
-	public static edu.stanford.hakan.aim4api.compability.aimv3.DICOMImageReference createDICOMImageReferenceV3Compability(String dsoStudyInstanceUID, String dsoSeriesInstanceUID,
-			String dsoSOPInstanceUID, String sopClassUID) 
-	{
-		edu.stanford.hakan.aim4api.compability.aimv3.DICOMImageReference dicomImageReference = new edu.stanford.hakan.aim4api.compability.aimv3.DICOMImageReference();
-		dicomImageReference.setCagridId(0);
-
-		edu.stanford.hakan.aim4api.compability.aimv3.ImageStudy imageStudy = new edu.stanford.hakan.aim4api.compability.aimv3.ImageStudy();
-		imageStudy.setCagridId(0);
-		imageStudy.setInstanceUID(dsoStudyInstanceUID);
-		imageStudy.setStartDate("2012-01-01T01:01:01"); // TODO
-		imageStudy.setStartTime("12:00:00"); // TODO
-
-		edu.stanford.hakan.aim4api.compability.aimv3.ImageSeries imageSeries = new edu.stanford.hakan.aim4api.compability.aimv3.ImageSeries();
-		imageSeries.setCagridId(0);
-		imageSeries.setInstanceUID(dsoSeriesInstanceUID);
-
-		edu.stanford.hakan.aim4api.compability.aimv3.Image image = new edu.stanford.hakan.aim4api.compability.aimv3.Image();
-		image.setCagridId(0);
-		log.info("written SOP Class UID=" + sopClassUID);
-		log.info("written SOP Instance UID=" + dsoSOPInstanceUID);
-			
-		image.setSopClassUID(sopClassUID); // TODO ml retrieved from parameter
-		image.setSopInstanceUID(dsoSOPInstanceUID);
-
-		imageSeries.addImage(image); // Add Image to ImageSeries
-		imageStudy.setImageSeries(imageSeries); // Add ImageSeries to ImageStudy
-		dicomImageReference.setImageStudy(imageStudy); // Add ImageStudy to ImageReference
-
-		return dicomImageReference;
-	}
+        
 	public static edu.stanford.hakan.aim4api.compability.aimv3.DICOMImageReference createDICOMImageReferenceV3Compability(String dsoStudyInstanceUID, String dsoSeriesInstanceUID,
 			String dsoSOPInstanceUID)
 	{
@@ -284,7 +253,7 @@ public class PluginAIMUtil
 
 		edu.stanford.hakan.aim4api.compability.aimv3.Image image = new edu.stanford.hakan.aim4api.compability.aimv3.Image();
 		image.setCagridId(0);
-		image.setSopClassUID("aa"); // TODO
+		image.setSopClassUID(""); // TODO
 		image.setSopInstanceUID(dsoSOPInstanceUID);
 
 		imageSeries.addImage(image); // Add Image to ImageSeries
