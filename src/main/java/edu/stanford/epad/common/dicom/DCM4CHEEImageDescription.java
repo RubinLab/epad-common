1146ae90591b6b1530a0ddda77b0cde8ec54c5b2
Emel Alkim <emelalkim@msn.com>
2016-05-17 17:44:29 -0700

    rescale slope and intercept added for each image

--- src/main/java/edu/stanford/epad/common/dicom/DCM4CHEEImageDescription.java
+++ src/main/java/edu/stanford/epad/common/dicom/DCM4CHEEImageDescription.java
@@ -110,6 +110,7 @@ public class DCM4CHEEImageDescription
 	public final int instanceNumber;
 	public final String sliceLocation, contentTime, updatedTime, createdTime, classUID;
 	public boolean multiFrameImage = false;
+	public final String rescaleIntercept, rescaleSlope;
 
 	public DCM4CHEEImageDescription(String studyUID, String seriesUID, String imageUID, int instanceNumber,
 			String sliceLocation, String contentTime, String updatedTime, String createdTime, String classUID)
@@ -123,5 +124,23 @@ public class DCM4CHEEImageDescription
 		this.updatedTime = updatedTime;
 		this.createdTime = createdTime;
 		this.classUID = classUID;
+		this.rescaleIntercept = "0";
+		this.rescaleSlope = "0";
+	}
+	
+	public DCM4CHEEImageDescription(String studyUID, String seriesUID, String imageUID, int instanceNumber,
+			String sliceLocation, String contentTime, String updatedTime, String createdTime, String classUID, String rescaleIntercept, String rescaleSlope)
+	{
+		this.studyUID = studyUID;
+		this.seriesUID = seriesUID;
+		this.imageUID = imageUID;
+		this.instanceNumber = instanceNumber;
+		this.sliceLocation = sliceLocation;
+		this.contentTime = contentTime;
+		this.updatedTime = updatedTime;
+		this.createdTime = createdTime;
+		this.classUID = classUID;
+		this.rescaleIntercept = rescaleIntercept;
+		this.rescaleSlope = rescaleSlope;
 	}
 }
