26962025794ac9401dabfb6efd3016f428b26725
Emel Alkim <emelalkim@msn.com>
2017-03-28 13:57:54 -0700

    added cie lab to rgb to convert the value in the header to rgb

--- src/main/java/edu/stanford/epad/common/pixelmed/DSOColorHelper.java
+++ src/main/java/edu/stanford/epad/common/pixelmed/DSOColorHelper.java
@@ -63,7 +63,87 @@ public class DSOColorHelper {
 	public static int[] rgb2ScaledLab(double[] rgb) {
 		return scaleLab4DSO(rgb2lab(rgb));
 	}
+
 	/*** end of rgb to lab conversion code********************************/
+	/*** lab to rgb ***********************************/
 	
+	public static int[] scaledLab2Rgb(int[] lab) {
+		return lab2rgb(unscaleLab4DSO(lab));
+	}
+	
+	public static double[] unscaleLab4DSO(int[] lab){
+		double[] scaled=new double[3];
+		scaled[0]= (((double)lab[0])*100.0/65535.0); //0:100
+		scaled[1]= ((((double)lab[1])*255.0/65535.0)-128); //-128:127
+		scaled[2]=((((double)lab[2])*255.0/65535.0)-128); //-128:127
+//		System.out.println("unscaled   ["+scaled[0]+" "+scaled[1]+" "+scaled[2]+"]");
+		//loosing some precision but the scaled value is the same and we can go to the same rgb
+//		int[] scaleagain=scaleLab4DSO(scaled);
+//		System.out.println("rescaled   ["+scaleagain[0]+" "+scaleagain[1]+" "+scaleagain[2]+"]");
+		return scaled;
+	}
+	
+	public static int[] lab2rgb(double[] lab){
+		return xyz2rgb(lab2xyz(lab));
+	}
+	
+	public static double ijkr(double r) {
+		return (r>0.008856)?(Math.pow(r,(3.0))):(((r-16.0/116.0)/7.787));
+	}
+	public static double[] lab2xyz(double[] lab){
+		//D65 Daylight reference
+		double xr=95.047;
+		double yr=100;
+		double zr=108.883;
+
+		double ak2=(lab[0]+16)/116;
+		double aj2=(lab[1]/500.0)+ak2;
+		double al2=ak2-lab[2]/200.0;
+		
+		double ag2=ijkr(aj2);
+		double ah2=ijkr(ak2);
+		double ai2=ijkr(al2);
+		
+		double x=ag2*xr;
+		double y=ah2*yr;
+		double z=ai2*zr;
+//		System.out.println("xyz   ["+x+" "+y+" "+z+"]");
+		
+		return new double[]{x,y,z};
+	}
+	
+	
+	public static double ader(double r) {
+		return (r>0.04045)?((Math.pow(r,(1.0/2.4))*1.055-0.055)):(r*12.92);
+	}
+	public static int[] xyz2rgb(double[] xyz){		
+		double a=41.24;
+		double b=35.76;
+		double c=18.05;
+		double d=-1*xyz[0];
+		double l=21.26;
+		double m=71.52;
+		double n=7.22;
+		double k=-1*xyz[1];
+		double p=1.93;
+		double q=11.92;
+		double r=95.05;
+		double s=-1*xyz[2];
+		double D = (a*m*r+b*p*n+c*l*q)-(a*n*q+b*l*r+c*m*p);
+		double ad2 = ((b*r*k+c*m*s+d*n*q)-(b*n*s+c*q*k+d*m*r))/D;
+		double ae2 = ((a*n*s+c*p*k+d*l*r)-(a*r*k+c*l*s+d*n*p))/D;
+		double af2 = ((a*q*k+b*l*s+d*m*p)-(a*m*s+b*p*k+d*l*q))/D;
+
+		int r2=(int) Math.round(ader(ad2)*255.0);
+		int g2=(int) Math.round(ader(ae2)*255.0);
+		int b2=(int) Math.round(ader(af2)*255.0);
+		
+//		System.out.println("rgb   ["+r2+" "+g2+" "+b2+"]");
+		
+		return new int[]{r2,g2,b2};
+		
+	}
+	
+	/***********end of lab to rgb *******************/
 
 }
